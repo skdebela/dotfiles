38159c0560b690a7d4bc482b0e28c929fefa54a9
Samuel M. Debela <113627346+skdebela@users.noreply.github.com>
2025-01-07 13:50:08 +0300

    Remove cavenv function from aliases

--- .shell/aliases.sh
+++ .shell/aliases.sh
@@ -25,33 +25,6 @@ jump() {
 	cd "$(dirname ${1})"
 }
 
-# create and activate a Python virtual environment
-cavenv() {
-    local input=${1:-./.venv}
-
-    # check if Python is installed
-    if ! command -v python &>/dev/null; then
-        echo "Python is not installed. Please install python first."
-        return 1
-    fi
-    
-    # create the virtual environment
-    if python -m venv "$input"; then
-        echo "Virtual environment create at $input."
-        # activate the virtual environment
-        source "$input/bin/activate"
-        echo "Virtual environment $input activated."
-    else
-        echo "Failed to create a virtual environment at $input."
-        return 1
-    fi
-}
-# Usage examples:
-# cavenv                Creates and activates the default virtual environment ./.venv
-# cavenv myvenv         Creates and activates virtual environment ./myvenv
-# cavenv ~/Envs/myvenv  Creates and activates virtual environment ~/Envs/myvenv
-
-
 
 # Jump to directory
 alias devt='cd ~/Documents/devt'
